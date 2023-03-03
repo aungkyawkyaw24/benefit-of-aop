e87878e8723fb2777abf6905ed5eb161dab1a034
aungkyawkyaw24 <aungkyawakk428@gmail.com>
2023-03-03 13:15:16 +0630

    benefit of aop

---
+++ no-aop/src/main/java/com/demo/reporsitoy/ReportProvider.java
@@ -0,0 +1,20 @@
+package com.demo.reporsitoy;
+
+import com.demo.ds.Report;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReportProvider {
+
+    public Report reportProvide(){
+
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Report Provider provide");
+        return new Report();
+    }
+}
